653ca999a9729ba5a2980fc6882b82bce7960dfc
Vladutaki <student@pcom-vm>
2024-04-24 14:27:39 +0300

    Frontend 1.0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,50 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import Signup from './Signup';
+import logo from './logo.png';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="header">
+        <div className="logo">
+        <img src={logo} alt="logo"></img>
+        </div>
+      </div>
+
+      <div className="main">  	
+        <input type="checkbox" id="chk" aria-hidden="true" />
+
+      <div className="signup">
+        <form>
+          <label htmlFor="chk">Sign up</label>
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="password" name="pswd" placeholder="Password" required />
+          <button>Sign up</button>
+        </form>
+      </div>
+
+      <div className="login">
+        <form>
+          <label htmlFor="chk">Login</label>
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="password" name="pswd" placeholder="Password" required />
+          <button>Login</button>
+        </form>
+      </div>
+      
+      </div>
     </div>
+
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+    </Router>
   );
 }
 
